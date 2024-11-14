dd5e6ecf4ef821b1c435c29cfc4d6575e1f7f0c4
Labadmin <bjh6070@psu.edu>
2024-11-14 11:21:17 -0500

    I got the home page to redirect non-logged in user to the login page...FINALLYY

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
-import {useLocation} from "react-router-dom";
+import React, {useState} from 'react';
+import {useLocation, Navigate} from "react-router-dom";
 
 
 
 const Home = () => {
+
+
     const location = useLocation();
     const data = location.state;
-    const loggedInUser = data.user;
 
+    if (!data || !data.user) {
+        return <Navigate to="/login" replace />;
+    }
+
+
+    const loggedInUser = data.user;
+    //This ensures that the user has user data before they can go to the home page
+
+
+
+    console.log(data.user);
 
     var googleClassroomRedirect = "http://localhost:3002/auth/google/"+loggedInUser.id;
+    console.log("USER DATA BELOW");
+    console.log(data);
+    console.log("USER DATA ABOVE");
 
     return (
+
         <div>
             <h1>My Home Screen</h1>
             <p>
@@ -20,6 +36,7 @@ const Home = () => {
                 <a href="http://localhost:3000/GradeReviewPage/7097162">View Canvas Grades</a>
             </p>
         </div>
+
     );
 };
 
